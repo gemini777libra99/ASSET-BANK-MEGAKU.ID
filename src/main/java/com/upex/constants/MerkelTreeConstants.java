9d52474abf52ad895b75796e0c6201ae964accc1
MERIANSYAH__Jack dani€£^$™ <herry.gemini777@outlook.co.id>
2026-02-23 11:37:15 +0800

    Initialize feature/v1.0.4

---
+++ src/main/java/com/upex/constants/MerkelTreeConstants.java
@@ -0,0 +1,15 @@
+package com.upex.constants;
+
+/**
+ * MerkelTreeConstants
+ * @author BitgetLimited
+ * @date 2022/11/25 23:03
+ */
+public class MerkelTreeConstants {
+
+    /**
+     * 逗号
+     */
+    public static final String COMMA = ",";
+
+}
